65d3abdf9b27ac8c7a4ffd52db01cbdc3b06cbd0
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2020-04-28 18:11:29 -0700

    Empty GameFragmentViewModel created

---
+++ app/src/main/java/gustavo/projects/dotaquiz/game/GameFragmentViewModel.kt
@@ -0,0 +1,12 @@
+package gustavo.projects.dotaquiz.game
+
+import androidx.lifecycle.ViewModel
+
+class GameFragmentViewModel : ViewModel() {
+
+    private lateinit var heroesMap: MutableMap<String, Array<String>>
+
+    init{
+        
+    }
+}
