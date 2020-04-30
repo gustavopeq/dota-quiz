2cec0c07c5648c69539aac14b78d9a9c3bd315ed
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2020-04-29 19:04:42 -0700

    GameFragmentVM created and heroesMap

--- app/src/main/java/gustavo/projects/dotaquiz/game/GameFragmentViewModel.kt
+++ app/src/main/java/gustavo/projects/dotaquiz/game/GameFragmentViewModel.kt
@@ -4,9 +4,34 @@ import androidx.lifecycle.ViewModel
 
 class GameFragmentViewModel : ViewModel() {
 
-    private lateinit var heroesMap: MutableMap<String, Array<String>>
+    private lateinit var heroesMap: HashMap<String, Array<String>>
+    private  lateinit var heroesList: MutableSet<String>
+
+    lateinit var heroSelected: String
 
     init{
-        
+        TODO("Create view model, starting on heroes hashmap")
+
+        createHeroesMap()
+
+        heroesList = heroesMap.keys
+    }
+
+    private fun createHeroesMap(){
+        heroesMap["Sniper"] = arrayOf("Headshot", "Dwarf", "Gun", "Shooter")
+        heroesMap["Drow Ranger"] = arrayOf("Ice", "Archer", "Slow", "Aura")
+        heroesMap["Gyrocopter"] = arrayOf("Helicopter", "Missile", "Stun", "Explosion")
+        heroesMap["Lina"] = arrayOf("Fire", "Hot", "Lightning", "Stun")
+        heroesMap["Storm Spirit"] = arrayOf("Lightning", "Blue", "Storm", "Mana")
+        heroesMap["Antimage"] = arrayOf("Mage", "Blade", "Blink", "Resistant")
+    }
+
+    private  fun nextHero(){
+        if(heroesList.isNotEmpty()){
+            heroesList.shuffled()
+
+            heroSelected = heroesList.elementAt(0)
+            heroesList.remove(0)
+        }
     }
 }
