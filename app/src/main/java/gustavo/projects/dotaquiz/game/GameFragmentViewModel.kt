3ca36c53d1d29f976355a4e6b1759324807827af
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2020-05-08 12:45:13 -0700

    More heroes added

--- app/src/main/java/gustavo/projects/dotaquiz/game/GameFragmentViewModel.kt
+++ app/src/main/java/gustavo/projects/dotaquiz/game/GameFragmentViewModel.kt
@@ -89,6 +89,18 @@ class GameFragmentViewModel : ViewModel() {
         heroesMap["Lina"] = arrayOf("Fire", "Hot", "Lightning", "Stun")
         heroesMap["Storm Spirit"] = arrayOf("Lightning", "Blue", "Storm", "Mana")
         heroesMap["Antimage"] = arrayOf("Mage", "Blade", "Blink", "Resistant")
+        heroesMap["Tiny"] = arrayOf("Rock", "Stone", "Tree", "Throw")
+        heroesMap["IO"] = arrayOf("Link", "Regeneration", "Round", "Blue")
+        heroesMap["Tusk"] = arrayOf("Snowball", "Punch", "Roll", "Ice")
+        heroesMap["Pudge"] = arrayOf("Hook", "Fat", "Meat", "Stink")
+        heroesMap["Magnus"] = arrayOf("Together", "Shock", "Cleave", "Tusk")
+        heroesMap["Mirana"] = arrayOf("Moon", "Arrow", "Tiger", "Stars")
+        heroesMap["Bounty Hunter"] = arrayOf("Shuriken", "Invisible", "Critical", "Money")
+        heroesMap["Zeus"] = arrayOf("God", "Lightning", "Thunder", "Heaven")
+        heroesMap["Invoker"] = arrayOf("Magic", "Sun", "Orbs", "Tornado")
+        heroesMap["Nature's Prophet"] = arrayOf("Trees", "Teleport", "Rat", "Split")
+        heroesMap["Silencer"] = arrayOf("Silence", "Mute", "Dispell", "Curse")
+        heroesMap["Omniknight"] = arrayOf("Heal", "Knight", "Guardian", "Repel")
     }
 
     //The HeroesList gets the name of each hero available, and is used during the game. Every time a hero is picked, it is removed from this list
