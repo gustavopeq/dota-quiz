6e4fe48607ec4ccf3deea733b6467a882830f8bb
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2020-05-19 11:52:39 -0700

    Coroutine dependencies added and database tested

--- app/src/main/java/gustavo/projects/dotaquiz/rank/RankFragmentViewModel.kt
+++ app/src/main/java/gustavo/projects/dotaquiz/rank/RankFragmentViewModel.kt
@@ -2,11 +2,54 @@ package gustavo.projects.dotaquiz.rank
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import gustavo.projects.dotaquiz.model.PlayerScore
 import gustavo.projects.dotaquiz.model.RankDatabaseDao
+import kotlinx.coroutines.*
 
-class RankFragmentViewModel(val dataSource: RankDatabaseDao) : ViewModel() {
+class RankFragmentViewModel(val database: RankDatabaseDao) : ViewModel() {
+
+    private var viewModelJob = Job()
+    private var uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
 
     init{
-        Log.i("Print", "Initialized")
+        uiScope.launch {
+
+            var test = getAllPlayers()
+
+            Log.i("print", test.size.toString())
+
+            //val newPlayerScore = PlayerScore(0,"Gustavo", 1000)
+            //insert(newPlayerScore)
+        }
+
+
+    }
+
+    private suspend fun getAllPlayers(): Array<PlayerScore>{
+        return withContext(Dispatchers.IO){
+            var result = database.getTop5()
+
+            return@withContext result
+        }
+    }
+
+    private suspend fun insert(newPlayerScore: PlayerScore) {
+        withContext(Dispatchers.IO){
+            database.insert(newPlayerScore)
+
+
+        }
+    }
+
+    fun checkDatabase(){
+        uiScope.launch {
+
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
